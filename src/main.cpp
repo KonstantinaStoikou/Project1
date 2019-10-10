79f1ad2f8775ee116f94160dfb44e1e0f41d13ab
Konna <tinasto97@gmail.com>
2019-10-10 14:55:45 +0300

    Make printing point a seperate function

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,25 @@
 #include "../include/hash_functions.h"
 #include "../include/read_functions.h"
 
+void print_vectors(std::vector<std::vector<int>> &points) {
+    // print vector of vectors with points
+    std::vector<std::vector<int>>::iterator pnt;
+    std::vector<int>::iterator dim;
+    for (pnt = points.begin(); pnt != points.end(); pnt++) {
+        std::cout << "New Point: ";
+        for (dim = pnt->begin(); dim != pnt->end(); dim++) {
+            std::cout << *dim << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main(int argc, char const *argv[]) {
     std::vector<std::vector<int>> points;
     if (read_vectors_file("../sample datasets/vectors/siftsmall/query_small_id",
                           points) < 0) {
         std::cout << "error";
     }
-    // // print vector of vectors with points
-    // std::vector<std::vector<int>>::iterator pnt;
-    // std::vector<int>::iterator dim;
-    // for (pnt = points.begin(); pnt != points.end(); pnt++) {
-    //     std::cout << "New Point: ";
-    //     for (dim = pnt->begin(); dim != pnt->end(); dim++) {
-    //         std::cout << *dim << " ";
-    //     }
-    //     std::cout << "\n";
-    // }
+    print_vectors(points);
     return 0;
 }
